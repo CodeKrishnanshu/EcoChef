987c3841f294367cd0856c9a2dda4b3c4c97ffb2
Krishnanshu Agrawal <krishnanshuagrawalcode@gmail.com>
2024-06-01 22:29:19 +0200

    Backend Initial Commit

---
+++ backend/ecoChefMiSe/src/main/java/org/example/ecochefmise/EcoChefMiSeApplication.java
@@ -0,0 +1,13 @@
+package org.example.ecochefmise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoChefMiSeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoChefMiSeApplication.class, args);
+    }
+
+}
